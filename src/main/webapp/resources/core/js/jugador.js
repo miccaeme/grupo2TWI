7c95c49497a45fb659875d7a10554f0f3ad89f68
Leandro <leandrocarrazcopedraza@gmail.com>
2026-05-20 00:10:25 -0300

    fix eslint

--- src/main/webapp/resources/core/js/jugador.js
+++ src/main/webapp/resources/core/js/jugador.js
@@ -1,22 +1,22 @@
 const posicionesPorDeporte = {
-    FUTBOL: ["ARQUERO", "DEFENSOR", "MEDIOCAMPISTA", "DELANTERO"],
-    BASQUET: ["BASE", "ESCOLTA", "ALERO", "ALA_PIVOT", "PIVOT"],
-    VOLEY: ["ARMADOR", "CENTRAL", "PUNTA", "OPUESTO", "LIBERO"],
-    PADEL: ["DRIVE", "REVES"]
+  FUTBOL: ["ARQUERO", "DEFENSOR", "MEDIOCAMPISTA", "DELANTERO"],
+  BASQUET: ["BASE", "ESCOLTA", "ALERO", "ALA_PIVOT", "PIVOT"],
+  VOLEY: ["ARMADOR", "CENTRAL", "PUNTA", "OPUESTO", "LIBERO"],
+  PADEL: ["DRIVE", "REVES"]
 };
 
 const deporteSelect = document.getElementById("deporte");
 const posicionSelect = document.getElementById("posicion");
 
 deporteSelect.addEventListener("change", function () {
-    posicionSelect.innerHTML = '<option value="">Seleccione una posici&oacute;n</option>';
+  posicionSelect.innerHTML = "<option value=\"\">Seleccione una posici&oacute;n</option>";
 
-    const posiciones = posicionesPorDeporte[this.value];
+  const posiciones = posicionesPorDeporte[this.value];
 
-    posiciones.forEach(function (posicion) {
-        const option = document.createElement("option");
-        option.value = posicion;
-        option.text = posicion;
-        posicionSelect.appendChild(option);
-    });
+  posiciones.forEach(function (posicion) {
+    const option = document.createElement("option");
+    option.value = posicion;
+    option.text = posicion;
+    posicionSelect.appendChild(option);
+  });
 });
