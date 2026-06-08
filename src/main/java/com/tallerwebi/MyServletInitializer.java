5a799c6ba0312510f8f4bdf809c46a16539a6c36
miccaeme <miccaeme16@gmail.com>
2026-06-07 22:54:07 -0300

    usuario+datos + fromt registrarse y loogin

--- src/main/java/com/tallerwebi/MyServletInitializer.java
+++ src/main/java/com/tallerwebi/MyServletInitializer.java
@@ -3,7 +3,11 @@ package com.tallerwebi;
 import com.tallerwebi.config.DatabaseInitializationConfig;
 import com.tallerwebi.config.HibernateConfig;
 import com.tallerwebi.config.SpringWebConfig;
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.Filter;
+
 
 public class MyServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
 
@@ -26,5 +30,13 @@ public class MyServletInitializer extends AbstractAnnotationConfigDispatcherServ
   @Override
   protected String[] getServletMappings() {
     return new String[] { "/" };
+  }
+
+  @Override
+  protected Filter[] getServletFilters() {
+    CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+    characterEncodingFilter.setEncoding("UTF-8");
+    characterEncodingFilter.setForceEncoding(true); // Fuerza a que los requests y responses usen UTF-8
+    return new Filter[] { characterEncodingFilter };
   }
 }
